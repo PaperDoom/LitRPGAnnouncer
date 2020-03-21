fc7b3cd789ea726c561e5f99b7e05a7549225fa8
PaperDoom <36348714+PaperDoom@users.noreply.github.com>
2020-03-21 02:32:24 -0500

    dumb.

--- functions/postChapter.js
+++ functions/postChapter.js
@@ -2,9 +2,9 @@ module.exports = async (client, chapter) => {
 
   try {
 
-    client.treehouseHook.send(`A new chapter of **${chapter.title}** has posted!\n${chapter.link}`) ;
+    //client.treehouseHook.send(`A new chapter of **${chapter.title}** has posted!\n${chapter.link}`) ;
     client.litrpgHook.send(`A new chapter of **${chapter.title}** has posted!\n${chapter.link}`)
-    
+
     console.log("Chapter Posted: ", chapter.title)
   } catch (e) {
     console.log(e)
