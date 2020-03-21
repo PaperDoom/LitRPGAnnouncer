f61e951e259812f852dc1e06b21ea3b21ef05427
PaperDoom <36348714+PaperDoom@users.noreply.github.com>
2020-03-21 13:11:47 -0500

    wording change.

--- functions/postChapter.js
+++ functions/postChapter.js
@@ -3,7 +3,7 @@ module.exports = async (client, chapter) => {
   try {
 
     //client.treehouseHook.send(`A new chapter of **${chapter.title}** has posted!\n${chapter.link}`) ;
-    client.litrpgHook.send(`A new chapter of **${chapter.title}** has posted!\n${chapter.link}`)
+    client.litrpgHook.send(`A new chapter has posted **${chapter.title}**!\n${chapter.link}`)
 
     console.log("Chapter Posted: ", chapter.title)
   } catch (e) {
