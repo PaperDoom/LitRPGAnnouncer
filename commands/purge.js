f786d33042622264d5e1a9887bafae72a6fd7aba
PaperDoom <36348714+PaperDoom@users.noreply.github.com>
2020-12-02 14:46:27 -0600

    added purge command

---
+++ commands/purge.js
@@ -0,0 +1,32 @@
+exports.run = async (client, message, args) => {
+
+    try {
+
+        let messagecount = parseInt(args[0], 10) ? parseInt(args[0], 10) : 1;
+        let messages = await message.channel.messages.fetch({ limit: 100 });
+        let msg_array = messages.array();
+
+        msg_array.length = messagecount;
+        msg_array.map(m => m.delete().catch(console.error));
+
+    }
+    catch (err) {
+
+        if (err) throw err;
+
+    }
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "Moderator"
+};
+
+exports.help = {
+    name: "prune",
+    category: "System",
+    description: "Deletes a number of bot messages.",
+    usage: "prune [number]"
+};
